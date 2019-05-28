fb9cc6c7aac92836d3a0032ae0d9a5daebea63f6
長野鈴 <suzu.nagano@maxmouse.co.jp>
2019-05-28 16:03:44 +0900

    [fix]css修正、gulpfile.jsの修正

--- gulpfile.js
+++ gulpfile.js
@@ -1,131 +1,139 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
+var watch = require('gulp-watch');
 var plumber = require('gulp-plumber'); //強制停止を防止
 var notify = require('gulp-notify'); //通知を表示
-var connect = require('gulp-connect'); //ローカルサーバ立ち上げ
 var browserSync = require('browser-sync'); //ブラウザシンク
 var ssi = require('connect-ssi'); //ssi
-
-var csscomb = require('gulp-csscomb'); // css整形
-var autoprefixer = require('gulp-autoprefixer'); // ベンダープレフィックス付与
+var runSequence = require('run-sequence');
+var crLfReplace = require ('gulp-cr-lf-replace');
+var convertEncoding = require ('gulp-convert-encoding');
+var pleeease = require('gulp-pleeease');
 var cache = require('gulp-cached');
-var cssmin = require('gulp-clean-css'); // css圧縮
-var jsmin = require('gulp-uglify'); // js圧縮
+var jsmin = require('gulp-uglify');
 
 var imagemin = require('gulp-imagemin'); // 画像圧縮
 var pngquant = require('imagemin-pngquant'); // 圧縮率を高めるのにプラグインを入れる png
 var mozjpeg = require('imagemin-mozjpeg'); // 圧縮率を高めるのにプラグインを入れる jpg
 
-// paths 
-var paths = {
-  srcDir: 'src',
-  dstDir: 'htdocs'
-}
-var srcGlob = paths.srcDir; //開発環境
-var dstGlob = paths.dstDir; //コンパイル先
+// paths
+var srcDir = 'src';
+var dstDir = 'htdocs';
+var targetDir = '';
+var targetPage = '';
 
-// css 
+
+// css
 gulp.task('sass', function() {
-  gulp.src(['src/**/*.scss', '!src/**/component/*.scss'])
-    .pipe(plumber({ // デスクトップに通知を出す
+  return gulp.src(srcDir + '/**/*.scss')
+    .pipe(plumber({
       errorHandler: notify.onError('<%= error.message %>')
     }))
-    .pipe(autoprefixer(['last 2 versions']))
-    .pipe(autoprefixer({ grid: true }))
-    .pipe(csscomb())
     .pipe(sass({
-      outputStyle: 'expanded'
+      outputStyle: 'expanded' // expanded: 展開 compressed : 圧縮
+    }))
+    .pipe(pleeease({
+      rem: {rootValue: '10px'},
+      autoprefixer: {
+        browsers: ['iOS 10', 'Android 4.4', 'last 2 version']
+      },
+      opacity: false,
+      minifier: false // 圧縮しない場合：false
     }))
     .pipe(cache())
-    .pipe(gulp.dest(dstGlob))
+    .pipe(crLfReplace({changeCode: 'CR+LF'}))// 改行コード変更
+    .pipe(convertEncoding({to: 'UTF-8'})) // 文字コード変更
+    .pipe(gulp.dest(dstDir))
 });
 
-//server 
-gulp.task('connect', function() {
-  connect.server({
-    root: paths.dstDir, //ルートディレクトリ
-    livereload: true //ライブリロード
-  });
-});
 
 // browserSync
 gulp.task('browserSync', function() {
-  browserSync({
+  return browserSync.init({
     server: {
-      baseDir: paths.dstDir, // ルートとなるディレクトリを指定
+      baseDir: dstDir, // ルートとなるディレクトリを指定
+      port: 3001,
       middleware: [
         ssi({
-          baseDir: dstGlob,
+          baseDir: dstDir,
           ext: '.html'
         })
       ]
-    }
+    },
+    startPath: 'index.html',
+    open: 'external',
+    notify: false
   });
 });
 
+
 //reload
 gulp.task('reload', function() {
-  gulp.src(paths.dstDir + '/**/*.css').pipe(connect.reload());
-  gulp.src(paths.dstDir + '/**/*.html').pipe(connect.reload());
   browserSync.reload();
 });
+
 
 //watchタスク
 gulp.task('watch', function() {
-  gulp.watch(paths.srcDir + '/**/*.scss', ['sass']);
-  gulp.watch(dstGlob + '/**/*.*', ['reload']);
+  watch("./**/*.scss", function(event) {
+    gulp.start("sass");
+  });
+  watch(dstDir + '/**/*.*', function(event) {
+    gulp.start("reload");
+  });
   browserSync.reload();
 });
 
-/// default ////////////////////////////////////////////
-gulp.task('default', ['watch', 'browserSync']);
 
-
-/// css comb ////////////////////////////////////////////
-gulp.task('csscomb', function() {
-  gulp.src(['../scss/**/*.scss'])
-    .pipe(autoprefixer(
-      ['last 2 versions']
-    ))
-    .pipe(csscomb())
-    .pipe(gulp.dest('../css/'))
+// default /////////////////////////////////////
+gulp.task('default', function(callback) {
+  return runSequence(
+    'browserSync',
+    'sass',
+    'watch',
+    callback
+  );
 });
 
-/// css min ////////////////////////////////////////////
-gulp.task("cssmin", function() {
-  var srcGlob = paths.srcDir + '/**/*.css';
-  var dstGlob = paths.dstDir;
-  gulp.src(srcGlob)
-    .pipe(cssmin())
-    .pipe(gulp.dest(dstGlob));
-});
-
-/// js min ////////////////////////////////////////////
+//js min
 gulp.task("jsmin", function() {
-  var srcGlob = paths.srcDir + '/**/*.js';
-  var dstGlob = paths.dstDir;
-  gulp.src(srcGlob)
+  return gulp.src(srcDir + '/**/' + targetDir + '/**/*.js')
+    .pipe(plumber())
     .pipe(jsmin())
-    .pipe(gulp.dest(dstGlob));
+    .pipe(gulp.dest(dstDir))
+    .pipe(notify('js minify finished'))
 });
 
-/// imagemin ////////////////////////////////////////////
-gulp.task('imagemin', function() {
-  var srcGlob = paths.srcDir + '/**/*.+(jpg|jpeg|png|gif)';
-  var dstGlob = paths.dstDir;
-  gulp.src(srcGlob)
+
+//imagemin
+gulp.task('imgmin', function() {
+  return gulp.src(srcDir + '/**/' + targetDir + '/**/*.{jpg,jpeg,png,gif}')
+    .pipe(plumber())
     .pipe(imagemin([
       pngquant({
         quality: '60-80',
         speed: 1,
         floyd: 0
       }),
-      imagemin.jpegtran({
+      mozjpeg({
         quality: 85,
         progressive: true
-      })
+      }),
+      imagemin.svgo(),
+      imagemin.optipng(),
+      imagemin.gifsicle()
     ]))
-    .pipe(imagemin())
-    .pipe(gulp.dest(dstGlob))
-    .pipe(notify('images task finished'));
+    .pipe(notify('image minified'))
+    .pipe(gulp.dest(dstDir))
+    .pipe(notify('image minify finished'))
+});
+
+
+// minify /////////////////////////////////////
+gulp.task('minify', function(callback) {
+  return runSequence(
+    'jsmin',
+    'imgmin',
+    callback
+  );
 });
